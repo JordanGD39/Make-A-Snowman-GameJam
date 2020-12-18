ef56a5cadc0369fde5f3c092c2a722e7b41e08fe
ihartog <26035@ma-web.nl>
2020-12-18 12:41:16 +0100

    Enemy
    working enemy

---
+++ Make-A-Snowman/Assets/Scripts/BasicEnemy.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BasicEnemy : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private GameObject enemy;
+    private GameObject player;
+    [SerializeField]
+    private float throwTimer;
+    [SerializeField]
+    private float throwDistance;
+    private float distanceToPlayer;
+    [SerializeField]
+    private GameObject fireball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = GameObject.Find("angry snowman");
+        player = GameObject.Find("Player");
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckDistance();
+        Throw();
+    }
+
+    void CheckDistance()
+    {
+       distanceToPlayer = player.transform.position.x - enemy.transform.position.x;
+    }
+
+    void Throw()
+    {
+        if (distanceToPlayer <= throwDistance)
+        {
+            throwTimer -= Time.deltaTime;
+
+            if (throwTimer <= 0)
+            {
+                Debug.Log(throwTimer);
+                Debug.Log("throwing");
+                Instantiate(fireball, enemy.transform.position, Quaternion.identity);
+                throwTimer = 5;
+            }
+        }
+    }
+}
---
+++ Make-A-Snowman/Assets/Scripts/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private float speed = 3;
+    private GameObject player;
+    Vector3 playerPos;
+    private bool posReached;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+        playerPos = player.transform.position;
+        transform.right = playerPos - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position += transform.right * speed * Time.deltaTime;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("hit");
+        Destroy(gameObject);
+    }
+}
