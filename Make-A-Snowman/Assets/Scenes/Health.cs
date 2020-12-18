f0489c6ec6105b28d9a121aa95068a99cc6bf5f5
JustinvdLans <26590@ma-web.nl>
2020-12-18 11:25:35 +0100

    yeet
    health, ui and sprites

---
+++ Make-A-Snowman/Assets/Scenes/Health.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+
+    public int playerHealth;
+
+    public int maxHealth;
+
+    public Image[] Hearts;
+    public Sprite fullHeart;
+    public Sprite halfHeart;
+
+    void Update()
+    {
+
+        if (playerHealth > maxHealth)
+        {
+            playerHealth = maxHealth;
+        }
+
+        for (int i = 0; i < Hearts.Length; i++)
+        {
+            if (i < playerHealth)
+            {
+                Hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                Hearts[i].sprite = halfHeart;
+            }
+
+            if (i < maxHealth)
+            {
+                Hearts[i].enabled = true;
+            }
+            else
+            {
+                Hearts[i].enabled = false;
+            }
+        }
+    }
+}
---
+++ Make-A-Snowman/Assets/Scenes/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("I Quit");
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void SettingsMenu()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void PausedGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void RestartGame()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
